649fb86099df6f6d02d94631e799bc39efbc5082
Mohanvijay8344 <Mohanvijay8344@gmail.com>
2023-10-05 10:49:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { PriceHistoryTable } from "./PriceHistoryTable";
 import { TotalRates } from "./TotalRates";
 import Button from '@mui/material/Button';
-import { Signin } from "./Signin";
 import { Signup } from "./Signup";
 import { Forgot } from "./Forgot";
 import { Reset } from "./Reset";
+import React from "react";
+import { useFormik } from "formik";
+import { API } from "./global";
 
 export default function App() {
   return (
@@ -65,6 +67,66 @@ function Error() {
         src="https://t3.ftcdn.net/jpg/02/29/46/30/360_F_229463039_B4KwKD6ifdRV8G5S0sqrzBrAfHNzpReq.jpg"
         alt="error"
       />
+    </div>
+  );
+}
+
+
+
+
+export function Signin() {
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      console.log(values);
+      const data = await fetch(`${API}/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      if (data.status === 401) {
+        console.log("error");
+        alert("Invalid Cretentials ❌❌")
+      } else {
+        console.log("success");
+        const result = await data.json();
+        console.log(result);
+        localStorage.setItem("token", result.token);
+        navigate("/home");
+        alert("Login Successfull ✅✅")
+
+      }
+    },
+  });
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <form className="form" onSubmit={formik.handleSubmit}>
+        <input
+          placeholder="username"
+          name="username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+        />
+        <input
+          placeholder="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <button type="submit">Sign In</button>
+        <label>
+          Not a member? <a href="/signup">Sign Up</a>
+        </label>
+        <a href="/forgot-password">Forgot Password</a>
+      </form>
     </div>
   );
 }
